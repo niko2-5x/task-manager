c8f110bfef744c675dfe5db78d9a391a65874d0f
Laura Xaraba <lauraxaraba@gmail.com>
2024-08-07 02:30:06 -0300

    hrml & js refactoring

--- app.js
+++ app.js
@@ -5,72 +5,91 @@
 //? armazenar tasks em display no navegador
 //Limpar input depois de usar addNewTask
 const taskList = document.getElementById("task-list");
-const delButton = document.getElementById;
-const defaultDelBtn = document.getElementsByClassName("default-del-button");
-const taskChecker = document.getElementsByClassName("task-checker");
-const taskContent = document.getElementsByClassName("task-text");
 
+var tasks = [
+	{ label: "Visit grandma", checked: false },
+	{ label: "Study Web Develpment", checked: true },
+]
 
 //	Function to create an entire new task.
 
-function addNewTask() {
-	if (!taskList) {
-		return;
-	}
+// 1 - Adicionar um item na lista (task.push(...item))
+// 2 - Renderizar cada item da lista tasks
 
-	let newTask = document.getElementById("task-input");
+// Essa funcão deve criar os elementos HTML correspondentes
+// a uma tarefa na lista (div, checkbox, label, button)
+// de acordo com os valores no objeto `task` (parametro da
+// função), e inserir a tarefa no DOM HTML
+function makeTaskElement (task = {
+	label: "",
+	checked: false
+}) {
 	let newDivTask = document.createElement("div");
 	newDivTask.classList.add("task");
+
 	let newDivFlex = document.createElement("div");
 	newDivFlex.classList.add("task-flex");
+
 	let newChecker = document.createElement("input");
 	newChecker.classList.add("task-checker");
 	newChecker.type = "checkbox";
+
 	let newTaskContent = document.createElement("label");
-	newTaskContent.textContent = newTask.value;
+	newTaskContent.textContent = task.label;
+	console.log({ task })
+
 	let newButton = document.createElement("button");
 	newButton.classList.add("delete-button");
 	newButton.textContent = "⨯";
 
-	taskList.appendChild(newDivTask);
+
 	newDivTask.appendChild(newDivFlex);
 	newDivFlex.appendChild(newChecker);
 	newDivFlex.appendChild(newTaskContent);
 	newDivTask.appendChild(newButton);
 
-//  Function called to delete the tasks created by pressing 
-//	the "x" button.
+	// - Task-List
+	//  - .task
+	//    - .task-flex
+	//     - <checkbox>
+	//     - <label>
+	//    - .button
 
-	newButton.addEventListener("click", () => 
-	{
-		if (newDivTask) {
-			taskList.removeChild(newDivTask);
-		};
-	});
 
-	newChecker.addEventListener("change", (event) =>
-	{
-		if (newChecker.checked) {
-			newTaskContent.classList.add("task-text-checked")
-		} else {
-			newTaskContent.classList.remove("task-text-checked")
-		}
+	return newDivTask;
+}
+
+// 1 - Quando for renderizada pela primeira vez
+// 2 - Quando novo item for adicionada
+
+function addNewTask() {
+	let newTask = document.getElementById("task-input");
+
+	tasks.push({
+		label: (newTask || {}).value || "",
+		checked: false
 	})
 
 	newTask.value = "";
 }
 
-//  Function called when the "x" button at the default tasks 
-//  are clicked, it deletes the intire task.
+function renderTaskList () {
+	const taskListEl = document.getElementById("task-list");
 
-function delDefaultTask() {
-	for (const element of defaultDelBtn){
-		if (element.parentElement){
-			element.parentElement.remove();
-		}
-	};
+	tasks.forEach((task) =>
+		taskListEl.appendChild(makeTaskElement(task)))
 }
 
-function checkedStyle() {
-
+function docReady(fn) {
+  if (
+		document.readyState === "complete" ||
+		document.readyState === "interactive"
+	) {
+        // call on next available tick
+    setTimeout(fn, 1);
+  } else {
+    document.addEventListener("DOMContentLoaded", fn);
+  }
 }
+
+docReady(renderTaskList)
