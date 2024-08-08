db510114ec068f74abba8fa0eec268c32b784f19
Laura Xaraba <lauraxaraba@gmail.com>
2024-08-07 22:09:19 -0300

    refatoring code, finishing JS

--- app.js
+++ app.js
@@ -1,95 +1,59 @@
-//variável para novas tasks
-//criar uma função para o botão de add-task
-//criar uma função para o botão de delete-task
-//Transformar o style dos text das tasks marcadas
-//? armazenar tasks em display no navegador
-//Limpar input depois de usar addNewTask
 const taskList = document.getElementById("task-list");
+const taskInput = document.getElementById("task-input");
 
-var tasks = [
-	{ label: "Visit grandma", checked: false },
-	{ label: "Study Web Develpment", checked: true },
-]
-
-//	Function to create an entire new task.
-
-// 1 - Adicionar um item na lista (task.push(...item))
-// 2 - Renderizar cada item da lista tasks
-
-// Essa funcão deve criar os elementos HTML correspondentes
-// a uma tarefa na lista (div, checkbox, label, button)
-// de acordo com os valores no objeto `task` (parametro da
-// função), e inserir a tarefa no DOM HTML
-function makeTaskElement (task = {
-	label: "",
-	checked: false
-}) {
-	let newDivTask = document.createElement("div");
-	newDivTask.classList.add("task");
-
-	let newDivFlex = document.createElement("div");
-	newDivFlex.classList.add("task-flex");
-
-	let newChecker = document.createElement("input");
-	newChecker.classList.add("task-checker");
-	newChecker.type = "checkbox";
-
-	let newTaskContent = document.createElement("label");
-	newTaskContent.textContent = task.label;
-	console.log({ task })
-
-	let newButton = document.createElement("button");
-	newButton.classList.add("delete-button");
-	newButton.textContent = "⨯";
-
-
-	newDivTask.appendChild(newDivFlex);
-	newDivFlex.appendChild(newChecker);
-	newDivFlex.appendChild(newTaskContent);
-	newDivTask.appendChild(newButton);
-
-	// - Task-List
-	//  - .task
-	//    - .task-flex
-	//     - <checkbox>
-	//     - <label>
-	//    - .button
-
-
-	return newDivTask;
-}
-
-// 1 - Quando for renderizada pela primeira vez
-// 2 - Quando novo item for adicionada
+//	Function to create an entire new task and save	//
+//	it at the local storage, so the page can be     //
+//	reloaded withou information loss.               //
 
 function addNewTask() {
-	let newTask = document.getElementById("task-input");
+	if (taskInput.value === "") {
+		alert("You must insert a task.");
+	} else {
+		let newTask = document.createElement("li");
+		newTask.innerHTML = taskInput.value;
+		
+		taskList.appendChild(newTask);
 
-	tasks.push({
-		label: (newTask || {}).value || "",
-		checked: false
-	})
+		let delButton = document.createElement("span");
+		delButton.innerHTML = "\u00d7";
 
-	newTask.value = "";
+		newTask.appendChild(delButton);
+	}
+
+	taskInput.value = "";
+	saveData();
 }
 
-function renderTaskList () {
-	const taskListEl = document.getElementById("task-list");
+//	Make possible for the user to add the new task 	//
+//	by pressing down the "Enter" key.              	//
 
-	tasks.forEach((task) =>
-		taskListEl.appendChild(makeTaskElement(task)))
+document.addEventListener("keydown", function(event) {
+	if (event.key === "Enter") {
+			addNewTask();
+	}
+});
+
+taskList.addEventListener("click", function(elClicked){
+	if (elClicked.target.tagName === "LI"){
+		elClicked.target.classList.toggle("checked-task");
+		saveData();
+	} 
+	else if (elClicked.target.tagName === "SPAN"){
+		elClicked.target.parentElement.remove();
+		saveData()
+	}
+}, false);
+
+//	Create a function to save the tasks at the list	//
+//	at the local storage.                         	//
+
+function saveData() {
+	localStorage.setItem("data", taskList.innerHTML);
 }
 
-function docReady(fn) {
-  if (
-		document.readyState === "complete" ||
-		document.readyState === "interactive"
-	) {
-        // call on next available tick
-    setTimeout(fn, 1);
-  } else {
-    document.addEventListener("DOMContentLoaded", fn);
-  }
-}
+//	Event that calls the previously stored items	  //
+//	by the moment the window loads.               	//
 
-docReady(renderTaskList)
+window.addEventListener('load', function() {
+	taskList.innerHTML = localStorage.getItem("data");
+});
